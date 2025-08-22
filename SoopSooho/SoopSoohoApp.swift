4c1f1c55ca651dbba05d134ee02dfff5ab7ccb09
hiseyeon <se020122@naver.com>
2025-08-23 00:31:35 +0900

    feat: 초기 프로젝트 생성

---
+++ SoopSooho/SoopSoohoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoopSoohoApp.swift
+//  SoopSooho
+//
+//  Created by Hwnag Seyeon on 8/23/25.
+//
+
+import SwiftUI
+
+@main
+struct SoopSoohoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
